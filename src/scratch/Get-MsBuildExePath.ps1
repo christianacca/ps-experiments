17772a29e599a883397c30a780787cc35d9fed96
christianacca <christian.crowhurst@gmail.com>
2018-01-21 15:16:53 +0000

    example Get-MsBuildExePath

---
+++ src/scratch/Get-MsBuildExePath.ps1
@@ -0,0 +1,16 @@
+functionGet-MsBuildExePath
+{
+        process
+{
+
+            $path =  Get-ChildItem "HKLM:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\" |
+                                Sort-Object {[double]$_.PSChildName} -Descending |
+                                Select-Object -First 1 |
+                                Get-ItemProperty -Name MSBuildToolsPath |
+                                Select -ExpandProperty MSBuildToolsPath
+
+            $path = (Join-Path -Path $path -ChildPath 'msbuild.exe')
+
+        return Get-Item $path
+    }
+}
