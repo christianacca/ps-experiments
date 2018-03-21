e84744f4c28c2f6707729d56ed4f2d7cddd633bf
christianacca <christian.crowhurst@gmail.com>
2018-03-21 17:17:01 +0000

    exception experiments

--- src/exceptions/ThrowingDemo/ThrowingModule.demo.ps1
+++ src/exceptions/ThrowingDemo/ThrowingModule.demo.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-Install-Module ThrowingModule -Repository LocalRepo
+# Install-Module ThrowingModule -Repository LocalRepo
 Import-Module ThrowingModule
 # Get-Module ThrowingModule -All | Remove-Module -Force
 # Import-Module "$PSScriptRoot\..\ThrowingModule"
@@ -10,5 +10,17 @@ Clear-Host
 Get-CaccaMaybeThrowResult '?'
 Write-Host 'ThrowingModule.demo.ps1... still running (1)'
 
+try {
+    Write-Host 'About to try and catch custom exception from Get-CaccaMaybeThrowResult'
+    Get-CaccaMaybeThrowResult '?' -EA 'Stop'    
+    Write-Host 'ThrowingModule.demo.ps1... still running (2)'
+}
+# catch [ThrowingModuleException] {
+#     Write-Host 'Able to catch custom exception'
+# }
+catch {
+    Write-Host 'Able to catch custom exception (kind of)'
+}
+
 Get-CaccaMaybeThrowResult '?' -EA 'Stop'
-Write-Host 'ThrowingModule.demo.ps1... still running (2)'
+Write-Host 'ThrowingModule.demo.ps1... still running (3)'
